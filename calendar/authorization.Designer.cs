227d1e638984270728d09545b2b610b57d51a565
koporok <122740702+koporok@users.noreply.github.com>
2024-05-20 20:17:26 +0300

    Добавьте файлы проекта.

---
+++ calendar/authorization.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace calendar
+{
+    partial class authorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.обои_authorization = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Clik_Pass = new System.Windows.Forms.Label();
+            this.Clik_Registration = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.heading = new System.Windows.Forms.Label();
+            this.обои_authorization.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // обои_authorization
+            // 
+            this.обои_authorization.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.обои_authorization.BackgroundImage = global::calendar.Properties.Resources.обои_ночь;
+            this.обои_authorization.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.обои_authorization.Controls.Add(this.label1);
+            this.обои_authorization.Controls.Add(this.pictureBox4);
+            this.обои_authorization.Controls.Add(this.pictureBox3);
+            this.обои_authorization.Controls.Add(this.button1);
+            this.обои_authorization.Controls.Add(this.textBox2);
+            this.обои_authorization.Controls.Add(this.textBox1);
+            this.обои_authorization.Controls.Add(this.pictureBox2);
+            this.обои_authorization.Controls.Add(this.pictureBox1);
+            this.обои_authorization.Controls.Add(this.Clik_Pass);
+            this.обои_authorization.Controls.Add(this.Clik_Registration);
+            this.обои_authorization.Controls.Add(this.label2);
+            this.обои_authorization.Controls.Add(this.heading);
+            this.обои_authorization.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.обои_authorization.ForeColor = System.Drawing.Color.Transparent;
+            this.обои_authorization.Location = new System.Drawing.Point(-8, -4);
+            this.обои_authorization.Name = "обои_authorization";
+            this.обои_authorization.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.обои_authorization.Size = new System.Drawing.Size(1096, 604);
+            this.обои_authorization.TabIndex = 0;
+            this.обои_authorization.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Niagara Solid", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(546, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 21);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "или";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = global::calendar.Properties.Resources.google;
+            this.pictureBox4.InitialImage = global::calendar.Properties.Resources.user;
+            this.pictureBox4.Location = new System.Drawing.Point(576, 445);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(48, 46);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::calendar.Properties.Resources.Vk;
+            this.pictureBox3.InitialImage = global::calendar.Properties.Resources.user;
+            this.pictureBox3.Location = new System.Drawing.Point(503, 445);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(47, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(182)))), ((int)(((byte)(79)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(136)))), ((int)(((byte)(79)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(51)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(136)))), ((int)(((byte)(79)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(434, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Войти ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(470, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(228, 29);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(470, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 29);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox2.Image = global::calendar.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(434, 261);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 34);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Image = global::calendar.Properties.Resources.user;
+            this.pictureBox1.InitialImage = global::calendar.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(432, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Clik_Pass
+            // 
+            this.Clik_Pass.AutoSize = true;
+            this.Clik_Pass.BackColor = System.Drawing.Color.Transparent;
+            this.Clik_Pass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Clik_Pass.Font = new System.Drawing.Font("Niagara Solid", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clik_Pass.ForeColor = System.Drawing.Color.Transparent;
+            this.Clik_Pass.Location = new System.Drawing.Point(587, 298);
+            this.Clik_Pass.Name = "Clik_Pass";
+            this.Clik_Pass.Size = new System.Drawing.Size(111, 17);
+            this.Clik_Pass.TabIndex = 3;
+            this.Clik_Pass.Text = "?Забыли пароль";
+            this.Clik_Pass.Click += new System.EventHandler(this.label3_Click);
+            this.Clik_Pass.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            this.Clik_Pass.MouseLeave += new System.EventHandler(this.Clik_Pass_MouseLeave);
+            // 
+            // Clik_Registration
+            // 
+            this.Clik_Registration.AutoSize = true;
+            this.Clik_Registration.BackColor = System.Drawing.Color.Transparent;
+            this.Clik_Registration.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Clik_Registration.Font = new System.Drawing.Font("Niagara Solid", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clik_Registration.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Clik_Registration.Location = new System.Drawing.Point(504, 533);
+            this.Clik_Registration.Name = "Clik_Registration";
+            this.Clik_Registration.Size = new System.Drawing.Size(120, 23);
+            this.Clik_Registration.TabIndex = 2;
+            this.Clik_Registration.Text = "Регистрация ";
+            this.Clik_Registration.Click += new System.EventHandler(this.Clik_Registration_Click);
+            this.Clik_Registration.MouseEnter += new System.EventHandler(this.Clik_Registration_MouseEnter);
+            this.Clik_Registration.MouseLeave += new System.EventHandler(this.Clik_Registration_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label2.Font = new System.Drawing.Font("Niagara Solid", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(486, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ваш Личный Календарь";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // heading
+            // 
+            this.heading.BackColor = System.Drawing.Color.Transparent;
+            this.heading.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.heading.Font = new System.Drawing.Font("Segoe Script", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.heading.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.heading.Location = new System.Drawing.Point(366, 100);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(410, 64);
+            this.heading.TabIndex = 0;
+            this.heading.Text = "Матрица Встреч";
+            this.heading.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.heading.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 595);
+            this.Controls.Add(this.обои_authorization);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "authorization";
+            this.Text = "Матрица встеч";
+            this.обои_authorization.ResumeLayout(false);
+            this.обои_authorization.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label heading;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Clik_Registration;
+        private System.Windows.Forms.Label Clik_Pass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        protected System.Windows.Forms.Panel обои_authorization;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ calendar/authorization.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calendar
+{
+    public partial class authorization : Form
+    {
+        public authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+       
+
+        private void об_authorization(object sender, PaintEventArgs e)
+        {
+            // Предполагается, что у вас есть пути к изображениям для утра, дня, вечера и ночи.
+            string morningImagePath = "images.обои_вечер.jpg";
+            string afternoonImagePath = "images.обои_день.jpg";
+            string eveningImagePath = "images.обои_вечер.jpg";
+            string nightImagePath = "images.обои_ночь.jpg";
+
+            DateTime currentTime = DateTime.Now;
+
+            if (currentTime.Hour >= 5 && currentTime.Hour < 12)  // Утро (с 5:00 до 11:59)
+            {
+                обои_authorization.BackgroundImage = Image.FromFile(morningImagePath);
+            }
+            else if (currentTime.Hour >= 12 && currentTime.Hour < 17)  // День (с 12:00 до 16:59)
+            {
+                обои_authorization.BackgroundImage = Image.FromFile(afternoonImagePath);
+            }
+            else if (currentTime.Hour >= 17 && currentTime.Hour < 20)  // Вечер (с 17:00 до 19:59)
+            {
+                обои_authorization.BackgroundImage = Image.FromFile(eveningImagePath);
+            }
+            else  // Ночь (с 20:00 до 4:59)
+            {
+                обои_authorization.BackgroundImage = Image.FromFile(nightImagePath);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            Clik_Pass.ForeColor = Color.Blue;
+        }
+
+        private void Clik_Pass_MouseLeave(object sender, EventArgs e)
+        {
+            Clik_Pass.ForeColor = Color.White;
+        }
+
+        private void Clik_Registration_MouseEnter(object sender, EventArgs e)
+        {
+            Clik_Registration.ForeColor = Color.Blue;
+        }
+
+        private void Clik_Registration_MouseLeave(object sender, EventArgs e)
+        {
+            Clik_Registration.ForeColor = Color.White;
+        }
+
+  
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clik_Registration_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
